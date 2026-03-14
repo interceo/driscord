7b093a35fece879240c918eadc32cdbae8bbab53
Your Name <you@example.com>
2026-03-14 20:20:10 +0300

    win fix

--- client/src/audio/system_audio_capture_win.cpp
+++ client/src/audio/system_audio_capture_win.cpp
@@ -2,15 +2,14 @@
 
 #ifdef _WIN32
 
-#include <Windows.h>
+// clang-format off
+#include <windows.h>
+#include <mmdeviceapi.h>
+#include <audioclient.h>
+// clang-format on
 
 #include <atomic>
 #include <thread>
-
-#include <audioclient.h>
-#include <functiondiscoverykeys_devpkey.h>
-#include <mmdeviceapi.h>
-#include <windows.h>
 
 #include "log.hpp"
 
