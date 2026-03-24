c6e946880e177df9b5df02185a44ca616a7d88ff
Internet Director <interceo@tumba.ch>
2026-03-24 15:35:23 +0300

    move screenshare to core

--- client/jni/screen_session_jni.hpp
+++ client/jni/screen_session_jni.hpp
@@ -4,17 +4,22 @@
 #include "video/screen_session.hpp"
 
 #include <mutex>
+#include <string>
 
 // Thin JNI adapter that owns a ScreenSession and exposes Java callbacks
 // (on_frame / on_frame_removed). All business logic lives in ScreenSession.
 struct ScreenSessionJni {
     ScreenSession session;
+    std::string   watched_peer_id;
 
     std::mutex  cb_mutex;
     JniCallback on_frame_cb;
     JniCallback on_frame_removed_cb;
 
     ScreenSessionJni(int buf_ms, int max_sync_ms);
+
+    void join_stream(const std::string& peer_id);
+    void leave_stream();
 
     void fire_frame(const std::string& peer_id, const uint8_t* rgba, int w, int h);
     void fire_remove_frame(const std::string& peer_id);
