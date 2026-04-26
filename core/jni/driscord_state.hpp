dfe251015ed235011352f988bbd084979d514011
Your Name <you@example.com>
2026-04-26 21:32:49 +0300

    remove kotlin-compose client

--- core/jni/driscord_state.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "driscord_core.hpp"
-
-// Global singleton: owns the DriscordCore instance.
-struct DriscordState {
-    DriscordCore core;
-    static DriscordState& get();
-};
